00b3f5e69765f1057ac3969b55c791229ec18215
natukiHw <reg@natuki.cc>
2023-07-13 00:48:53 +0800

    2023.07.12: 默认流程节点和自定义节点注册简单测试

--- src/views/g6/example/index.vue
+++ src/views/g6/example/index.vue
@@ -1,14 +1,31 @@
 <template>
   <div class="container">
     <!-- 节点面板 -->
-    <div class="node_panel">
-      <div v-for="item in nodeList" :key="item" class="node_li">
-        <div
-          :class="['item_shape', item]"
-          draggable="true"
-          @dragend="addNode(item, $event)"
-        />
-        <div>{{ item }}</div>
+    <!--    <div class="node_panel">-->
+    <!--      <div v-for="item in nodeList" :key="item" class="node_li">-->
+    <!--        <div-->
+    <!--          :class="['item_shape', item]"-->
+    <!--          draggable="true"-->
+    <!--          @dragend="addNode(item, $event)"-->
+    <!--        />-->
+    <!--        <div>{{ item }}</div>-->
+    <!--      </div>-->
+    <!--    </div>-->
+    <div class="nodePanel">
+      <div class="panelTitle">节点面板</div>
+      <div class="panelContent">
+        <el-collapse v-model="nodePanelExpandArr">
+          <el-collapse-item v-for="group in nodeList" :key="group.key" :title="group.title" :name="group.key">
+            <el-row>
+              <el-col :span="12" v-for="item in group.child" :key="item.nodeType">
+                <div class="nodeBtn" draggable="true" @dragend="addNode(item.nodeType, $event)">
+                  <IconifyIconOnline :icon="item.nodeIcon" />
+                  <div class="nodeName">{{ item.nodeName }}</div>
+                </div>
+              </el-col>
+            </el-row>
+          </el-collapse-item>
+        </el-collapse>
       </div>
     </div>
     <!-- 画板 -->
@@ -65,7 +82,174 @@ function initGraph() {
 }
 
 // 节点列表
-const nodeList = ["rect", "circle"];
+const nodeList = [
+  {
+    key: "graphicGroup",
+    title: "图形节点",
+    child: [
+      {
+        nodeName: "圆形",
+        nodeType: "circle",
+        nodeIcon: "carbon:executable-program"
+      },
+      {
+        nodeName: "矩形",
+        nodeType: "rect",
+        nodeIcon: "ph:rectangle-bold"
+      }
+    ]
+  },
+  {
+    key: "processGroup",
+    title: "流程节点",
+    child: [
+      {
+        nodeName: "启动",
+        nodeType: "startCard",
+        nodeIcon: "gridicons:create"
+      },
+      {
+        nodeName: "审批",
+        nodeType: "approvalCard",
+        nodeIcon: "akar-icons:chat-approve"
+      },
+      {
+        nodeName: "条件",
+        nodeType: "conditionCard",
+        nodeIcon: "ph:git-branch"
+      },
+      {
+        nodeName: "执行",
+        nodeType: "executionCard",
+        nodeIcon: "carbon:executable-program"
+      }
+    ]
+  }
+];
+// 节点面板展开列表
+const nodePanelExpandArr = ref([]);
+for (const item of nodeList) {
+  nodePanelExpandArr.value.push(item.key);
+}
+
+// 注册节点
+const ICON_MAP = {
+  startCard: 'http://127.0.0.1:8848/src/assets/user.jpg',
+  approvalCard: 'http://127.0.0.1:8848/src/assets/user.jpg',
+};
+G6.registerNode(
+  'startCard',
+  {
+    drawShape: function drawShape(cfg, group) {
+      const color = cfg.error ? '#F4664A' : '#30BF78';
+      const r = 2;
+      const shape = group.addShape('rect', {
+        attrs: {
+          x: 0,
+          y: 0,
+          width: 200,
+          height: 60,
+          stroke: color,
+          radius: r,
+        },
+        // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+        name: 'main-box',
+        draggable: true,
+      });
+
+      group.addShape('rect', {
+        attrs: {
+          x: 0,
+          y: 0,
+          width: 200,
+          height: 20,
+          fill: color,
+          radius: [r, r, 0, 0],
+        },
+        // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+        name: 'title-box',
+        draggable: true,
+      });
+
+      // left icon
+      group.addShape('image', {
+        attrs: {
+          x: 4,
+          y: 2,
+          height: 16,
+          width: 16,
+          cursor: 'pointer',
+          img: ICON_MAP[cfg.nodeType || 'app'],
+        },
+        // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+        name: 'node-icon',
+      });
+
+      // title text
+      group.addShape('text', {
+        attrs: {
+          textBaseline: 'top',
+          y: 2,
+          x: 24,
+          lineHeight: 20,
+          text: cfg.title,
+          fill: '#fff',
+        },
+        // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+        name: 'title',
+      });
+
+      if (cfg.nodeLevel > 0) {
+        group.addShape('marker', {
+          attrs: {
+            x: 184,
+            y: 30,
+            r: 6,
+            cursor: 'pointer',
+            symbol: cfg.collapse ? G6.Marker.expand : G6.Marker.collapse,
+            stroke: '#666',
+            lineWidth: 1,
+          },
+          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+          name: 'collapse-icon',
+        });
+      }
+
+      // The content list
+      cfg.panels.forEach((item, index) => {
+        // name text
+        group.addShape('text', {
+          attrs: {
+            textBaseline: 'top',
+            y: 25,
+            x: 24 + index * 60,
+            lineHeight: 20,
+            text: item.title,
+            fill: 'rgba(0,0,0, 0.4)',
+          },
+          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+          name: `index-title-${index}`,
+        });
+
+        // value text
+        group.addShape('text', {
+          attrs: {
+            textBaseline: 'top',
+            y: 42,
+            x: 24 + index * 60,
+            lineHeight: 20,
+            text: item.value,
+            fill: '#595959',
+          },
+          // must be assigned in G6 3.3 and later versions. it can be any string you want, but should be unique in a custom item type
+          name: `index-value-${index}`,
+        });
+      });
+      return shape;
+    },
+  },
+  'single-node',
+);
 
 // 添加节点
 function addNode(type, e) {
@@ -75,10 +259,17 @@ function addNode(type, e) {
   // 新创建的节点信息
   const model = {
     id: "node" + Math.random(), // id使用了随机数，尽可能避免重复
-    label: type, // 文本标签
+    title: type, // 文本标签
     type: type, // 图片类型的节点
+    nodeType: type,
     x: point.x,
-    y: point.y
+    y: point.y,
+    nodeLevel: 0,
+    panels: [
+      { title: '成功率', value: '11%' },
+      { title: '耗时', value: '111' },
+      { title: '错误数', value: '111' },
+    ]
   };
 
   graph.value.addItem("node", model, false);
@@ -107,15 +298,47 @@ onMounted(() => {
   background-color: #fff;
 }
 
-.node_panel {
+.nodePanel {
   position: absolute;
   left: 10px;
   top: 10px;
+  width: 150px;
   box-shadow: 0 0 4px rgba(0, 0, 0, 0.3);
   box-sizing: border-box;
-  padding: 10px 4px;
   border-radius: 4px;
   z-index: 10;
+
+  .panelTitle {
+    background-color: rgba(0, 0, 0, .2);
+    padding: 4px 10px;
+  }
+
+  .panelContent {
+    padding: 10px;
+
+    ::v-deep .el-collapse-item__content {
+      padding-bottom: 0;
+    }
+
+    .nodeBtn {
+      display: flex;
+      flex-direction: column;
+      align-items: center;
+      transition: all .3s;
+      padding: 8px 0;
+      cursor: pointer;
+      border-radius: 6px;
+
+      &:hover {
+        background-color: rgba(0, 0, 0, .1);
+      }
+
+      .iconify {
+        width: 20px;
+        height: 20px;
+      }
+    }
+  }
 }
 
 .node_li {
